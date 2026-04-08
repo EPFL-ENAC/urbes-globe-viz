19bbb30c775a3f625a9a6554604db54709c7bfdc
glin <guo-shiuan.lin@epfl.ch>
2026-04-08 10:53:30 +0200

    feat: add car_road_length project

---
+++ frontend/src/config/projects/car_road_length.ts
@@ -0,0 +1,72 @@
+import type { ProjectConfig } from "./types";
+
+const baseUrl = import.meta.env.DEV
+  ? "/geodata"
+  : "https://enacit4r-cdn-s3.epfl.ch/urbes-viz";
+
+export const lenOtherCarRoadsProject: ProjectConfig = {
+  id: "len_other_car_roads",
+  coordinates: [6.58, 46.41], // Lausanne
+
+  title: "Car road length",
+  description: "Aggregated car road length processed from SwissTLM3D - input to DAVE for car capacities",
+  category: "Transport",
+  year: 2025,
+  preview: "car_road_length.png",
+  zoom: 9,
+  pitch: 70,
+
+  unit: "meters/500m²",
+  info: "Source: SwissTLM3D, STATPOP",
+
+  source: {
+    type: "vector",
+    url: `pmtiles://${baseUrl}/len_other_car_roads.pmtiles`,
+  },
+  layer: {
+    id: "len_other_car_roads-layer",
+    type: "fill-extrusion",
+    source: "len_other_car_roads",
+    "source-layer": "other_car_roads",
+    // filter: [">=", ["get", "hour_12"], 5],
+    paint: {
+      "fill-extrusion-height": ["interpolate",["linear"], ["get", "sum"], 0,0,18000,30000],
+      "fill-extrusion-base": 0,
+      "fill-extrusion-opacity": 0.8,
+      "fill-extrusion-color": [
+        "interpolate",
+        ["linear"],
+        ["get", "sum"],
+        0,
+        "#ffffcc",
+        2000,
+        "#ffeda0",
+        4000,
+        "#fed976",
+        8000,
+        "#feb24c",
+        12000,
+        "#fd8d3c",
+        16000,
+        "#f03b20",
+        20000,
+        "#bd0026",
+      ],
+    },
+  },
+  legend: {
+    title: "Car road length (except autoway)",
+    gradient: {
+      stops: [
+        { value: "0", color: "#ffffcc" },
+        { value: "2'000", color: "#ffeda0" },
+        { value: "4'000", color: "#fed976" },
+        { value: "8'000", color: "#feb24c" },
+        { value: "12'000", color: "#fd8d3c" },
+        { value: "16'000", color: "#f03b20" },
+        { value: "20'000", color: "#bd0026" },
+      ],
+      unit: "meters/500m²",
+    },
+  },
+};
